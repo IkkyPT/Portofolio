6f28062a6d73de61ef950afefa60adb2c53f86f5
IkkyPT <ricardoo1998carvalho@gmail.com>
2024-01-27 14:24:42 +0000

    Basic structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  entry: './src/linkedList.js',
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
