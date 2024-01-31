99deeeccd77e2564e82e3c72e358ece74c23c18c
IkkyPT <ricardoo1998carvalho@gmail.com>
2024-01-31 14:41:43 +0000

    Refactor carousel to implement swiperJS.

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,44 +1,29 @@
-const imageWrapper = document.querySelector('.imageWrapper');
-// Select all child elements of '.imageWrapper' and store them in 'imageItems'
-const imageItems = document.querySelectorAll('.imageWrapper > *');
-// Set the number of items to display at a time
-const perView = 3;
-// Set the interval for automatic scrolling
-const delay = 2000;
-
-// Set a CSS custom property '--per-view' on '.imageWrapper' to determine the number of images to display at once
-imageWrapper.style.setProperty('--per-view', perView);
-
-for (let i = 0; i < perView; i++) {
-  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML);
-}
-
-let totalScroll = 0;
-
-// Set up an interval for automatic scrolling
-let autoScroll = setInterval(scrolling, delay);
-
-// Function to handle scrolling
-function scrolling() {
-  totalScroll++;
-
-  if (totalScroll === imageItems.length + 1) {
-    resetScroll();
-  }
-
-
-  // Function to reset the scroll position and restart the interval
-  function resetScroll() {
-    clearInterval(autoScroll);
-    totalScroll = 1;
-    imageWrapper.style.transition = '0s';
-    imageWrapper.style.left = '0';
-    autoScroll = setInterval(scrolling, delay);
-  }
-
-// Get the width of the first child element to calculate the scroll distance
-  const widthEl = document.querySelector('.imageWrapper > :first-child').offsetWidth + 24;
-  imageWrapper.style.left = `-${totalScroll * widthEl}px`;
-  imageWrapper.style.transition = '.3s';
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000, // Set the delay in milliseconds (3 seconds in this example)
+      disableOnInteraction: false, // Allow navigation while autoplaying
+    },
   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+});
