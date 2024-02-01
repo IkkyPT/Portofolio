ac2e68439bc1b64ab9da4319a07f4f17883a2e85
IkkyPT <ricardoo1998carvalho@gmail.com>
2024-02-01 16:07:25 +0000

    Made changes to update the right theme

--- src/scripts/daynight.js
+++ src/scripts/daynight.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const cicle = document.querySelector('.themeToggle');
 
     cicle.addEventListener('click', () => {
-        const moon = document.getElementById("moon");
-        if (moon.className === "moon") {
-            moon.className = "sun";
-            document.body.classList.remove("nightMode");
-            document.body.classList.add("dayMode");
-            document.body.style.transition = '1.2s';
-        } else {
+        const moon = document.getElementById("sun");
+        if (moon.className === "sun") {
             moon.className = "moon";
             document.body.classList.remove("dayMode");
             document.body.classList.add("nightMode");
-            document.body.style.transition = '1.2s';
+            document.body.style.transition = '0.2s';
+        } else {
+            moon.className = "sun";
+            document.body.classList.remove("nightMode");
+            document.body.classList.add("dayMode");
+            document.body.style.transition = '0.2s';
         }
     });
 });
